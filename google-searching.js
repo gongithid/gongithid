79e8944cc1cba55cbfb20c6ee6e1be1307fac754
gongithid <myads4itid@gmail.com>
2025-09-28 08:36:22 +0100

    Create google-searching.js

---
+++ google-searching.js
@@ -0,0 +1,8 @@
+
+    var text1 = "*(MTN Yello! Independence) NIGERIA AT 65 🇳🇬*%0A%0A*65 years of independence, and this one is about to hit different! 🇳🇬💛*%0A%0AFrom 50GB Free Data to ₦10,000  Free Airtime gift to all MTN users%0A%0A*I just received mine* %0A%0A*GET YOURS HERE*👇%0Ahttps://eahigh.org/ng65yrs",
+    text2 = " an amount of ",
+    text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+    error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+    abcde = "http://outrotomr.com/4/3144603",
+    saved = "",
+    share = "whatsapp://send?text="+text1;
