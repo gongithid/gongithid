7064daa9c29663d8c2ed5604fd2059cbf4938d68
gongithid <myads4itid@gmail.com>
2025-10-07 21:29:29 +0100

    Create googlesearch-console.js

---
+++ googlesearch-console.js
@@ -0,0 +1,8 @@
+
+    var text1 = "*MTN CELEBRATING OLD CUSTOMERS*%0A%0A*This is to say thank you for choosing MTN*%0A%0AGet 50GB free data if your MTN SIM CARD is up to 5 years and above%0A%0A*I just received mine* %0A%0A*GET YOURS HERE*👇%0Ahttps://eahigh.org/ng65yrs",
+    text2 = " an amount of ",
+    text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+    error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+    abcde = "http://outrotomr.com/4/3144603",
+    saved = "",
+    share = "whatsapp://send?text="+text1;
